8add527467081a80827173a09024c548c986fa15
aysenursezgin <115412288+aysenursezgin@users.noreply.github.com>
2024-07-18 15:43:24 +0300

    Proje dosyası ekle.

---
+++ IfElseYapilari/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Buffers;
+
+namespace ifElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan büyüktür");
+            }
+            else if (sayi < 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan küçüktür");
+            }
+            else if (sayi == 10)
+            {
+                Console.WriteLine("Girilen sayı 10'a eşittir");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz giriş");
+            }
+
+
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Girilen sayi cifttir.");
+            }
+
+            else { Console.WriteLine("Girilen sayi tektir"); }
+
+           
+        }
+    }
+}
+
+
